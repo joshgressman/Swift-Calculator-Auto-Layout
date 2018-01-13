1dd43e48c3915a8d615c14b82fe0243a40177627
joshgressman <joshgressman@gmail.com>
2018-01-13 12:57:54 -0600

    first Commit

---
+++ Calculator Auto Layout Stack View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator Auto Layout Stack View
+//
+//  Created by Josh Gressman on 1/13/18.
+//  Copyright © 2018 Josh Gressman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
